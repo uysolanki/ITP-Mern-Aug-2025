b36369230ba0e709c11726057fa3f5b6dfca3e16
uysolanki <uysolanki@gmail.com>
2025-12-26 12:51:55 +0530

    adding styles

--- javascriptprograms/day5/script2.js
+++ javascriptprograms/day5/script2.js
@@ -1,19 +1,33 @@
 function showText()
 {
 
-let textBox=document.getElementById("text1")
-let textBoxData=textBox.value.trim();
+let textBox=document.getElementById("text1")  //refer Textbox
+let textBoxData=textBox.value.trim();         // data
 if(textBoxData.length==0)
 {
     alert("Please enter a value")
+    textBox.value=""
+    textBox.focus()
     return
 }
 let liElement=document.createElement("li");   //<li></li>
 liElement.innerText = textBoxData;                   //<li>Audi</li>
 
 
-let deleteButton=document.createElement("button");
-deleteButton.innerText="Delete"
+let deleteButton=document.createElement("button"); //<button></button>
+deleteButton.innerText="Delete"                     //<button>Delete</button>
+//deleteButton.style.backgroundColor="red"
+//deleteButton.classList.add("add-button")
+let s1={
+    'background-color': 'blue',
+    'border-radius': '2px',
+    'border': 'none',
+    'padding': '5px',
+    'font-size': '16px',
+    'font-family': 'Courier New, Courier, monospace',
+    'color':'white'
+}
+ Object.assign(deleteButton.style,s1)
 liElement.appendChild(deleteButton)
 
 let olElment=document.getElementById("one")
