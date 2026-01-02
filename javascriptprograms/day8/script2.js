1b565b4094902266524d35c60a7d81489d4f1874
uysolanki <uysolanki@gmail.com>
2026-01-02 12:37:58 +0530

    context menu

--- javascriptprograms/day8/script2.js
+++ javascriptprograms/day8/script2.js
@@ -1,36 +1,60 @@
-document.addEventListener("keydown", saveDoc)
+// document.addEventListener("keydown", saveDoc)
 
-function saveDoc(event)
+// function saveDoc(event)
+// {
+//     //if(event.ctrlKey && event.key==='s')
+//     //if(event.altKey && event.key==='s')
+//     if(event.shiftKey && event.key==='s')
+//     {
+//         event.preventDefault()
+//         alert('Document Saved')
+//     }
+// }
+
+// const checkBoxElement=document.querySelector('#agree')
+
+// checkBoxElement.addEventListener("change", displayAgree)
+
+// function displayAgree()
+// {
+// console.log(checkBoxElement.checked)
+// }
+
+
+// const genderRadioButtons=document.querySelectorAll('input[name="gender"]')
+// console.log(genderRadioButtons)     //[0] [1]
+
+// genderRadioButtons.forEach(
+//     (gen)=>{
+//         gen.addEventListener("change", displayGender)
+//     }
+// )
+
+// function displayGender(event)
+// {
+// console.log(event.target.value)
+// }
+
+
+// document.addEventListener("contextmenu", rightClickDemo)
+
+// function rightClickDemo(event)
+// {
+// event.preventDefault()
+// alert('Right Click on Webpage')
+// }
+
+
+const divELement=document.querySelector('#div1')
+const targetUL=document.querySelector('#target1')
+
+divELement.addEventListener("contextmenu", displaySaveAs)
+
+function displaySaveAs(event)
 {
-    //if(event.ctrlKey && event.key==='s')
-    //if(event.altKey && event.key==='s')
-    if(event.shiftKey && event.key==='s')
-    {
-        event.preventDefault()
-        alert('Document Saved')
-    }
-}
+    event.preventDefault()
+    targetUL.style.display='block'
+    targetUL.style.left=`${event.pageX} px`
+    targetUL.style.top=`${event.pageY} px`
 
-const checkBoxElement=document.querySelector('#agree')
-
-checkBoxElement.addEventListener("change", displayAgree)
-
-function displayAgree()
-{
-console.log(checkBoxElement.checked)
-}
-
-
-const genderRadioButtons=document.querySelectorAll('input[name="gender"]')
-console.log(genderRadioButtons)     //[0] [1]
-
-genderRadioButtons.forEach(
-    (gen)=>{
-        gen.addEventListener("change", displayGender)
-    }
-)
-
-function displayGender(event)
-{
-console.log(event.target.value)
 }
