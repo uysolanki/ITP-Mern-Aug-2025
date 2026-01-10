ec0c31f7a5216e692985e34c7071ca80cfd68fed
uysolanki <uysolanki@gmail.com>
2026-01-10 11:36:19 +0530

    promise

---
+++ javascriptprograms/day10/script1.js
@@ -0,0 +1,34 @@
+function display(value)
+{
+    document.querySelector("#one").innerHTML=value;
+}
+
+let myPromise=new Promise(
+
+    function(myResolve,myReject)
+    {
+        let age=parseInt(prompt("Enter Age"))
+
+        if(age>=18)
+        {
+                myResolve('You are Adult')
+        }
+        else
+        {
+                myReject('You are Minor')
+        }
+    }
+)
+
+
+myPromise.then(
+     function(fulfilled)
+     {
+        display(fulfilled)
+     },
+
+      function(broken)
+     {
+        display(broken)
+     }
+)
