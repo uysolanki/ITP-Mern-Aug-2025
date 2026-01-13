f77d7309ae88ac34cba537b293fe36762bc92061
uysolanki <uysolanki@gmail.com>
2026-01-13 13:38:46 +0530

    react day 1

---
+++ ReactPrograms/project1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+
+      <p>React Mongo SQL</p>
+
+
+    </div>
+  )
+}
+
+export default App
