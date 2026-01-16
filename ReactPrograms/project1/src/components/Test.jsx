a11aba29d6a07e65503a07177854899b907d5d5c
uysolanki <uysolanki@gmail.com>
2026-01-16 12:52:21 +0530

    products

--- ReactPrograms/project1/src/components/Test.jsx
+++ ReactPrograms/project1/src/components/Test.jsx
@@ -3,20 +3,30 @@ import { products } from '../data/data'
 import './Test.css'
 const Test = () => {
     console.log(products)
-    const productTitles=products.map(
-        (product)=>(product.title)
+    const productTitles = products.map(
+        (product) => (product.title)
     )
     console.log(productTitles)
-  return (
-   <>
-   <h1>Products Page</h1>
-   {
-    productTitles.map(
-        (product)=>(<p>{product}</p>)
+    return (
+        <>
+        <div className='parent-container'>
+        {
+         products.map(
+            (product)=>{
+                return <div className="card" style={{'width':'18rem'}}>
+                <img className="card-img-top" src={product.image} alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">{product.description}</p>
+                    <a href="#" className="btn btn-primary">ADD TO CART</a>
+                </div>
+            </div>
+            }
+         )   
+        }
+        </div>
+        </>
     )
-   }
-   </>
-  )
 }
 
 export default Test
