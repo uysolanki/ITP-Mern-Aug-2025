7e8aeafc8e9bdfd21e92522fc8d3ea86ab9bae41
uysolanki <uysolanki@gmail.com>
2026-02-09 13:19:40 +0530

    initial commit

--- ReactPrograms/project1/src/App.jsx
+++ ReactPrograms/project1/src/App.jsx
@@ -1,21 +1,20 @@
+import { lazy, Suspense } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Menubar2 from './components/Menubar2'
-import Homepage from './pages/Homepage'
-import Login from './pages/Login'
-import RegisterForm from './pages/RegisterForm'
-import ShowProducts from './pages/ShowProducts'
-import SinglePage from './pages/SinglePage'
-import ShowProducts1 from './pages/ShowProducts1'
-import SinglePage1 from './pages/SinglePage1'
-import Demo from './components/Demo'
-import ShowProducts2 from './pages/ShowProducts2'
-import SinglePage2 from './pages/SinglePage2'
 
 const App = () => {
+  const Homepage = lazy(() => import('./pages/Homepage'))
+  const Demo = lazy(() => import('./pages/Login'))
+  const RegisterForm = lazy(() => import('./pages/RegisterForm'))
+  const ShowProducts2 = lazy(() => import('./pages/ShowProducts2'))
+  const SinglePage2 = lazy(() => import('./pages/SinglePage2'))
+
+
   return (
     <>
     <Router>
         <Menubar2/>
+        <Suspense fallback={<h1>Loading page...</h1>}>
         <Routes>
             <Route path="/"  element={<Homepage/>}/>
             <Route path="/login"  element={<Demo/>}/>
@@ -23,6 +22,7 @@ const App = () => {
             <Route path="/shop"  element={<ShowProducts2/>}/>
             <Route path="/single/:prodid"  element={<SinglePage2/>}/>
         </Routes>
+        </Suspense>
     </Router>
     </>
   )
